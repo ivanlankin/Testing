c4ccd175d270dfc6e4fe844141bfa4b685427b46
ivanlankin <34132072+ivanlankin@users.noreply.github.com>
2021-12-13 16:35:26 +0400

    Add corect

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -33,32 +33,33 @@ namespace AutomatedTests
             driver.FindElement(By.XPath("//*[contains(@class, 'ui-collapse__content')]//div[2]/input")).SendKeys(Keys.Enter);
             new WebDriverWait(driver, TimeSpan.FromSeconds(5))
                 .Until(x => driver.FindElements(By.CssSelector(".catalog-preloader__spin")).Count == 0);
-
+        
             int[] actualValues = Array.ConvertAll(driver.FindElements(By.XPath("//*[contains(@class,'catalog-products')]//*[@class='product-buy__price']"))
                .Select(webPrice => webPrice.Text.Replace(" ", "").Trim('₽')).ToArray<string>(), s => int.Parse(s));
             actualValues.ToList().ForEach(actualPrice => Assert.True(actualPrice >= 20000 && actualPrice <= 40000, "Price filter works wrong. Actual price is " + actualPrice + ". But should be more or equal than 20000 and less or equal than 40000"));
         }
-
+        
         [Test]
         public void TestTooltipText()
         {
-            new Actions(driver).MoveToElement(driver.FindElement(By.XPath("[contains(@class, 'wishlist-btn')]"))).Build().Perform();
-            Assert.IsTrue(driver.FindElements(By.XPath("//*[@class='products-list']//*[@class='button-ui buy-btn button-ui_brand']")).Any(),
+            driver.FindElement(By.XPath("//*[@class='menu-desktop__root'][4]//a[2]")).Click();
+            new Actions(driver).MoveToElement(driver.FindElement(By.XPath("//*[contains(@class,'catalog-product__buy')]//*[contains(@class, 'wishlist-btn')]"))).Build().Perform();
+            Assert.IsTrue(driver.FindElements(By.XPath("//*[contains(@class,'catalog-product__buy')]//*[@class='button-ui buy-btn button-ui_brand']")).Any(),
                 "Tooltip has not appeared.");
             Assert.AreEqual("Добавить в избранное", driver.FindElement(By.XPath("//*[@class='tooltip-ui tooltip-ui_top']")).Text.Trim(),
                 "Tooltip has not appeared.");
         }
 
-        //[Test]
-        //public void NegativeSignUpTest()
-        //{
-        //    driver.FindElement(By.CssSelector(".AuthPopup__button")).Click();
-        //    driver.FindElement(By.CssSelector(".AuthGroup__tab-sign-up")).Click();
-        //    driver.FindElement(By.CssSelector(".js--SignUp__input-name__container-input")).SendKeys("Test");
-        //    driver.FindElement(By.CssSelector(".js--SignUp__input-email__container-input")).SendKeys("vfbdhjsk57bs442@mail.ru");
-        //    Assert.IsFalse(driver.FindElements(By.XPath("//button[contains(@class, 'SignUp__button-confirm-phone') and not(@disabled)]")).Any(),
-        //        "Phone number confirmation button is enabel when phone number input has no value.");
-        //}
+        [Test]
+        public void NegativeSignUpTest()
+        {
+            driver.FindElement(By.CssSelector(".header__login_button")).Click();
+            driver.FindElement(By.CssSelector(".base-ui-input-row__input")).Click();
+            driver.FindElement(By.CssSelector(".base-ui-input-row__input")).SendKeys("vfbdhjsk57bs442");
+            driver.FindElement(By.XPath("//*[@class='form-entry-or-registry__main-button']//*[contains(@class,'base-ui-button')]")).Click();
+            Assert.AreEqual("E-mail/телефон указан неверно", driver.FindElement(By.CssSelector(".error-message-block.form-entry-or-registry__error")).Text.Trim(),
+                    "Registration is possible when phone number input has incorrect value.");
+        }
 
         [TearDown]
         public void CleanUp()
